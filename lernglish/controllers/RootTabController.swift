5501901da3eb0ebbc46f9de3d39f08973c076f5e
Maxim Belsky <public.belsky@gmail.com>
2017-03-15 23:04:20 +0300

    Set windows root controller

---
+++ lernglish/controllers/RootTabController.swift
@@ -0,0 +1,16 @@
+//
+//  RootTabController.swift
+//  lernglish
+//
+//  Created by Maxim Belsky on 15/03/2017.
+//  Copyright © 2017 Maxim Belsky. All rights reserved.
+//
+
+import UIKit
+
+class RootTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
