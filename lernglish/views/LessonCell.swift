8c967309a78229ac670bae1431b02055ebbe5647
Maxim Belsky <public.belsky@gmail.com>
2017-03-17 01:23:43 +0300

    Implement cell view to present lessons theme

---
+++ lernglish/views/LessonCell.swift
@@ -0,0 +1,40 @@
+//
+//  LessonCell.swift
+//  lernglish
+//
+//  Created by Maxim Belsky on 16/03/2017.
+//  Copyright © 2017 Maxim Belsky. All rights reserved.
+//
+
+import UIKit
+
+class LessonCell: UICollectionViewCell {
+    let label: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        return label
+    }()
+    fileprivate let viewSeparator: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.lightGray
+        return view;
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        addSubview(label)
+        addSubview(viewSeparator)
+
+        addConstraintsWithFormat("H:|-16-[v0]-16-|", views: label)
+        addConstraintsWithFormat("V:|-8-[v0]-8-[v1(1)]|", views: label, viewSeparator)
+        addConstraintsWithFormat("H:|[v0]|", views: viewSeparator)
+    }
+}
