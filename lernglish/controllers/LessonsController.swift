8bb5f9b8496fe704fe673905193fc063c46cbe74
Maxim Belsky <public.belsky@gmail.com>
2017-03-17 01:27:28 +0300

    Display lessons themes on screen

--- lernglish/controllers/LessonsController.swift
+++ lernglish/controllers/LessonsController.swift
@@ -8,9 +8,53 @@
 
 import UIKit
 
-class LessonsController: UIViewController {
+class LessonsController: UICollectionViewController {
+    fileprivate let cellId = "LessonsCellId"
+    fileprivate var
+    orderedCategories = ["Present Simple", "Past Simple"],
+    themes = ["Present Simple": ["Use", "Questions and negatives"], "Past Simple": ["Use", "Questions and negatives", "Irregular Verb Forms"]]
     
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(LessonCell.self, forCellWithReuseIdentifier: cellId)
+    }
+}
+
+// Implement protocols
+extension LessonsController: UICollectionViewDelegateFlowLayout {
+    private func getThemes(_ section: Int) -> [String] {
+        return themes[orderedCategories[section]] ?? [String]()
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return orderedCategories.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return getThemes(section).count;
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! LessonCell
+        cell.label.text = getThemes(indexPath.section)[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.bounds.width, height: 48)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
