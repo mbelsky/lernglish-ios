1caea2e6c14bf9574ea132e69872cd8aec0ba412
Maxim Belsky <public.belsky@gmail.com>
2017-03-17 03:03:18 +0300

    Create constants file

---
+++ lernglish/helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  lernglish
+//
+//  Created by Maxim Belsky on 17/03/2017.
+//  Copyright © 2017 Maxim Belsky. All rights reserved.
+//
+
+import UIKit
+
+struct K {
+    static let primaryColor = UIColor(html: 0x22B13B)!
+    
+    struct Font {
+        static let tableHeader = UIFont.systemFont(ofSize: 12.5, weight: 0.25)
+        static let tableRow = UIFont.systemFont(ofSize: 16.5, weight: 0)
+    }
+}
