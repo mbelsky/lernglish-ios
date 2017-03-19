c25b82910c67ad4a98ff0b284da199b36023c7a9
Maxim Belsky <public.belsky@gmail.com>
2017-03-19 06:53:17 +0300

    Display lessons content

--- lernglish/controllers/LessonController.swift
+++ lernglish/controllers/LessonController.swift
@@ -8,10 +8,18 @@
 
 import CoreData
 import UIKit
+import WebKit
 
 class LessonController: UIViewController {
     var sectionName: String?
     weak var theme: ThemeMO?
+
+    fileprivate var textView: UITextView!
+
+    override func loadView() {
+        textView = UITextView(frame: .zero)
+        view = textView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +30,33 @@ class LessonController: UIViewController {
 
         navigationItem.prompt = sectionName
         navigationItem.title = theme?.name
+
+        guard let html = wrapContent(theme?.content) else {
+            closeController()
+            return
+        }
+
+        DispatchQueue.global(qos: .userInteractive).async {
+            if let data = html.data(using: .unicode, allowLossyConversion: true),
+                    let text = try? NSAttributedString(data: data,
+                                                       options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
+                                                       documentAttributes: nil) {
+                DispatchQueue.main.async {
+                    self.textView.attributedText = text
+                }
+            }
+        }
     }
     
     func closeController() {
         dismiss(animated: true, completion: nil)
+    }
+
+    private func wrapContent(_ content: String?) -> String? {
+        if let content = content {
+            return "<html><body>" + content + "</body></html>"
+        } else {
+            return nil
+        }
     }
 }
