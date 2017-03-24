3fe32ec62e61abe9d64757d3e0dc49869a2b4f62
Maxim Belsky <public.belsky@gmail.com>
2017-03-24 18:01:46 +0300

    Implement tests paging

--- lernglish/controllers/TestsSetController.swift
+++ lernglish/controllers/TestsSetController.swift
@@ -8,18 +8,113 @@
 
 import UIKit
 
-class TestsSetController: UIPageViewController {
+class TestsSetController: UIViewController {
 
-    var tests: [TestMO]?
+    var tests: [TestMO]!
+
+    private var testControllers = [TestController]()
+
+    private var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.gestureRecognizers = nil
+        view.isPagingEnabled = true
+        view.showsHorizontalScrollIndicator = false
+        view.showsVerticalScrollIndicator = false
+        view.scrollsToTop = false
+        return view
+    }()
+
+    private var currentPage = -1 {
+        didSet {
+            displayPage(currentPage)
+        }
+    }
+    private var pagesCount: Int {
+        return tests.count + 1
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self,
                                                            action: #selector(closeController))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self,
+                                                            action: #selector(showNext))
+
+        view.backgroundColor = .white
+        view.addSubview(scrollView)
+
+        view.addConstraintsWithFormat("H:|[v0]|", views: scrollView)
+        scrollView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if 0 == testControllers.count {
+            scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(pagesCount), height: view.frame.height)
+            currentPage = 0
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        try? StorageHelper.instance.save()
     }
 
     func closeController() {
         dismiss(animated: true, completion: nil)
+    }
+
+    func showNext() {
+        currentPage += 1
+    }
+
+    private func displayPage(_ page: Int) {
+        loadPage(page - 1)
+        loadPage(page)
+        loadPage(page + 1)
+
+        // Update scroll view content offset
+        var bounds = scrollView.bounds
+        bounds.origin.x = bounds.width * CGFloat(page)
+        bounds.origin.y = 0
+        scrollView.scrollRectToVisible(bounds, animated: true)
+
+        navigationItem.title = pagesCount - 1 > page ? "\(page + 1) of \(pagesCount - 1)" : "Results"
+    }
+
+    private func loadPage(_ page: Int) {
+        guard -1 < page && page < pagesCount else {
+            // something went wrong
+            return
+        }
+
+        let controller: TestController
+        if testControllers.count > page {
+            controller = testControllers[page]
+        } else {
+            controller = TestController()
+            testControllers.insert(controller, at: page)
+        }
+
+        if nil == controller.view.superview {
+            var controllerFrame = scrollView.frame
+            controllerFrame.origin.x = controllerFrame.width * CGFloat(page)
+            controllerFrame.origin.y = 0
+            controller.view.frame = controllerFrame
+
+            addChildViewController(controller)
+            scrollView.addSubview(controller.view)
+            controller.didMove(toParentViewController: self)
+        }
+    }
+}
+
+class TestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
     }
 }
