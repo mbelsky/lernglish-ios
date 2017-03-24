b01004ae68772f84ae31d845ee016e91813a16a3
Maxim Belsky <public.belsky@gmail.com>
2017-03-24 04:50:45 +0300

    Add Practise controller cap

---
+++ lernglish/controllers/PracticeController.swift
@@ -0,0 +1,25 @@
+//
+//  PracticeController.swift
+//  lernglish
+//
+//  Created by Maxim Belsky on 24/03/2017.
+//  Copyright © 2017 Maxim Belsky. All rights reserved.
+//
+
+import UIKit
+
+class PracticeController: UIViewController {
+
+    var tests: [TestMO]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self,
+                                                           action: #selector(closeController))
+    }
+
+    func closeController() {
+        dismiss(animated: true, completion: nil)
+    }
+}
