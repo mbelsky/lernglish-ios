38fa9b6d9f8e7e63d8b292380328ebf2fec2cff1
Maxim Belsky <public.belsky@gmail.com>
2017-03-26 09:32:31 +0300

    Add some font constants

--- lernglish/helpers/Constants.swift
+++ lernglish/helpers/Constants.swift
@@ -19,8 +19,11 @@ struct K {
     }
     
     struct Font {
+        static let `default` = UIFont.systemFont(ofSize: 16.5, weight: 0)
+        static let emojiLarge = UIFont.systemFont(ofSize: 120, weight: 0)
         static let tableHeader = UIFont.systemFont(ofSize: 12.5, weight: 0.25)
-        static let tableRow = UIFont.systemFont(ofSize: 16.5, weight: 0)
+        static let tableRow = Font.default
+        static let title = UIFont.systemFont(ofSize: 17, weight: 0.5)
     }
 
     struct Test {
