1ac385647484b44b40749ab02776d8247a100a70
Maxim Belsky <public.belsky@gmail.com>
2017-03-28 07:30:59 +0300

    Display a label when no theme is discovered

--- lernglish/controllers/StartPracticeController.swift
+++ lernglish/controllers/StartPracticeController.swift
@@ -14,7 +14,7 @@ class StartPracticeController: UIViewController {
         let btn = UIButton()
         btn.backgroundColor = K.Color.primary
         btn.contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        btn.layer.cornerRadius = 2
+        btn.isHidden = true
         btn.setTitleColor(UIColor.white, for: .normal)
         btn.translatesAutoresizingMaskIntoConstraints = false
 
@@ -23,13 +23,34 @@ class StartPracticeController: UIViewController {
 
         return btn
     }()
+    private let lblEmpty: UILabel = {
+        let lbl = UILabel()
+        lbl.font = K.Font.default
+        lbl.isHidden = true
+        lbl.numberOfLines = 0
+        lbl.textAlignment = .center
+        lbl.textColor = K.Color.primaryDark
+        lbl.text = "Read a few themes to test your knowledge"
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
 
     private var tests: [TestMO]?
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(btnStart)
-        btnStart.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        for subview in [lblEmpty, btnStart] {
+            view.addSubview(subview)
+            subview.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        }
+
+        lblEmpty.bottomAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.addConstraintsWithFormat("H:|-16-[v0]-16-|", views: lblEmpty)
+
+        view.addConstraintsWithFormat("H:|-60-[v0]-60-|", views: btnStart)
+        let btnHeightConstraint = NSLayoutConstraint(item: btnStart, attribute: .height, relatedBy: .equal,
+                                                     toItem: btnStart, attribute: .width, multiplier: 1, constant: 0)
+        btnStart.addConstraint(btnHeightConstraint)
         btnStart.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(startPractice))
@@ -40,13 +61,21 @@ class StartPracticeController: UIViewController {
         super.viewWillAppear(animated)
         StorageHelper.instance.getTests { [weak self] tests in
             self?.tests = tests
-            self?.btnStart.isEnabled = true
+            if let tests = tests, tests.count > 0 {
+                self?.btnStart.isHidden = false
+                self?.lblEmpty.isHidden = true
+            } else {
+                self?.lblEmpty.isHidden = false
+                self?.btnStart.isHidden = true
+            }
         }
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        btnStart.isEnabled = false
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if 0 == btnStart.layer.cornerRadius {
+            btnStart.layer.cornerRadius = btnStart.bounds.width / 2
+        }
     }
 
     func startPractice() {
